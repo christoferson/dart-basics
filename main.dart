c00473fca6ff91a9cdef83b25eacdb6e860d95aa
christoferson <sweetsorrow1981@yahoo.co.jp>
2021-11-28 17:32:38 +0900

    Function: Arrow Notation Multiply By Nine

--- main.dart
+++ main.dart
@@ -4,9 +4,14 @@ int multiply(int x, int y) {
   return x * y;
 }
 
+// Arrow syntax is shorthand for `{ return expr; }`.
+int timesFour(int x) => timesTwo(timesTwo(x));
+int multiplyByNine(int x) => multiply(multiply(x, 3), 3);
+
 void demoFunctions() {
 
     print('5 times 3 is ${multiply(5, 3)}');
+    print('5 times 9 is ${multiplyByNine(5)}');
 
 }
 
