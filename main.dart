f5cebb906fcdfda9d9b249a4491fb509920c9986
christoferson <sweetsorrow1981@yahoo.co.jp>
2021-11-28 17:40:13 +0900

    Function: Function as Argument print

--- main.dart
+++ main.dart
@@ -5,14 +5,18 @@ int multiply(int x, int y) {
 }
 
 // Arrow syntax is shorthand for `{ return expr; }`.
-int timesFour(int x) => timesTwo(timesTwo(x));
 int multiplyByNine(int x) => multiply(multiply(x, 3), 3);
+
+// Functions are objects.
+String format(String title, int Function(int, int) f, int x, int y) {
+  return '${title} ${f(x, y)}';
+}
 
 void demoFunctions() {
 
     print('5 times 3 is ${multiply(5, 3)}');
     print('5 times 9 is ${multiplyByNine(5)}');
-
+    print(format("Call Function multiply with 3 and 7: ", multiply, 3, 7));
 }
 
 void main() {
