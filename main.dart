3dd1fdcf8cb177e250886e7a0a1aa2b749ee573e
christoferson <sweetsorrow1981@yahoo.co.jp>
2021-11-28 17:28:54 +0900

    Function: Simple Function multiply

--- main.dart
+++ main.dart
@@ -1,3 +1,15 @@
+
+// A function declaration.
+int multiply(int x, int y) {
+  return x * y;
+}
+
+void demoFunctions() {
+
+    print('5 times 3 is ${multiply(5, 3)}');
+
+}
+
 void main() {
-  print('Hello, World!');
+  demoFunctions();
 }
